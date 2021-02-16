5f688a7f2367842d7be36a738ff99026041d33da
Tarcio Rodrigo <tr.rodrigo@gmail.com>
2021-02-16 09:26:03 -0300

    Implementação de Autorização

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,13 +3,17 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { ReturnUserDto } from './dto/return-user.dto';
 import { UsersService } from './users.service';
 import { AuthGuard } from '@nestjs/passport';
+import { UserRole } from './users-roles.enum';
+import { Role } from 'src/auth/role.decorator';
+import { RolesGuard } from 'src/auth/roles.guard';
 
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService){}
 
     @Post()
-    @UseGuards(AuthGuard())
+    @Role(UserRole.ADMIN)
+    @UseGuards(AuthGuard(), RolesGuard)
     async createAdminUser(
         @Body(ValidationPipe) createUserDto: CreateUserDto,
     ): Promise<ReturnUserDto> {
