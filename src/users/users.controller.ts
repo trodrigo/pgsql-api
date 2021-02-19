5887d138a14c2422b3c2f149b4dd4c68c4429a83
trodrigo <tr.rodrigo@gmail.com>
2021-02-19 15:42:38 -0300

    Implementação do métodos Update, Delete e Get By Id

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, ForbiddenException, Get, Param, Patch, Post, Query, UseGuards, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { ReturnUserDto } from './dto/return-user.dto';
 import { UsersService } from './users.service';
@@ -6,14 +6,18 @@ import { AuthGuard } from '@nestjs/passport';
 import { UserRole } from './users-roles.enum';
 import { Role } from 'src/auth/role.decorator';
 import { RolesGuard } from 'src/auth/roles.guard';
+import { User } from './user.entity';
+import { UpdateUserDto } from './dto/update-users.dto';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { FindUsersQueryDto } from './dto/find-users-query.dto';
 
 @Controller('users')
+@UseGuards(AuthGuard(), RolesGuard)
 export class UsersController {
     constructor(private usersService: UsersService){}
 
     @Post()
     @Role(UserRole.ADMIN)
-    @UseGuards(AuthGuard(), RolesGuard)
     async createAdminUser(
         @Body(ValidationPipe) createUserDto: CreateUserDto,
     ): Promise<ReturnUserDto> {
@@ -23,4 +27,48 @@ export class UsersController {
             message: 'Administrador cadastrado com sucesso',
         };
     }
+
+    @Get(':id')
+    @Role(UserRole.ADMIN)
+    async findUserById(@Param('id') id): Promise<ReturnUserDto> {
+      const user = await this.usersService.findUserById(id);
+      return {
+        user,
+        message: 'Usuário encontrado',
+      };
+    }   
+    
+    @Patch(':id')
+    async updateUser(
+      @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+      @GetUser() user: User,
+      @Param('id') id: string,
+    ) {
+      if (user.role != UserRole.ADMIN && user.id.toString() != id) {
+        throw new ForbiddenException(
+          'Você não tem autorização para acessar esse recurso',
+        );
+      } else {
+        return this.usersService.updateUser(updateUserDto, id);
+      }
+    }    
+
+    @Delete(':id')
+    @Role(UserRole.ADMIN)
+    async deleteUser(@Param('id') id: string) {
+      await this.usersService.deleteUser(id);
+      return {
+        message: 'Usuário removido com sucesso',
+      };
+    }   
+    
+    @Get()
+    @Role(UserRole.ADMIN)
+    async findUsers(@Query() query: FindUsersQueryDto) {
+      const found = await this.usersService.findUsers(query);
+      return {
+        found,
+        message: 'Usuários encontrados',
+      };
+    }    
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,22 +1,67 @@
-import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
+import { FindUsersQueryDto } from './dto/find-users-query.dto';
+import { UpdateUserDto } from './dto/update-users.dto';
 import { User } from './user.entity';
 import { UserRole } from './users-roles.enum';
 import { UserRepository } from './users.repository';
 
 @Injectable()
 export class UsersService {
-    constructor(
-        @InjectRepository(UserRepository)
-        private userRepository: UserRepository,
-    ) {}
+  constructor(
+    @InjectRepository(UserRepository)
+    private userRepository: UserRepository,
+  ) { }
 
-    async createAdminUser(createUserDto: CreateUserDto): Promise<User> {
-        if (createUserDto.password != createUserDto.passwordConfirmation) {
-          throw new UnprocessableEntityException('As senhas não conferem');
-        } else {
-          return this.userRepository.createUser(createUserDto, UserRole.ADMIN);
-        }
-    }    
+  async createAdminUser(createUserDto: CreateUserDto): Promise<User> {
+    if (createUserDto.password != createUserDto.passwordConfirmation) {
+      throw new UnprocessableEntityException('As senhas não conferem');
+    } else {
+      return this.userRepository.createUser(createUserDto, UserRole.ADMIN);
+    }
+  }
+
+  async findUserById(userId: string): Promise<User> {
+    const user = await this.userRepository.findOne(userId, {
+      select: ['email', 'name', 'role', 'id'],
+    });
+
+    if (!user) throw new NotFoundException('Usuário não encontrado');
+
+    return user;
+  }
+
+  async updateUser(updateUserDto: UpdateUserDto, id: string): Promise<User> {
+    const user = await this.findUserById(id);
+    const { name, email, role, status } = updateUserDto;
+    user.name = name ? name : user.name;
+    user.email = email ? email : user.email;
+    user.role = role ? role : user.role;
+    user.status = status === undefined ? user.status : status;
+    try {
+      await user.save();
+      return user;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Erro ao salvar os dados no banco de dados',
+      );
+    }
+  }
+
+  async deleteUser(userId: string) {
+    const result = await this.userRepository.delete({ id: userId });
+    if (result.affected === 0) {
+      throw new NotFoundException(
+        'Não foi encontrado um usuário com o ID informado',
+      );
+    }
+  }
+
+  async findUsers(
+    queryDto: FindUsersQueryDto,
+  ): Promise<{ users: User[]; total: number }> {
+    const users = await this.userRepository.findUsers(queryDto);
+    return users;
+  }
 }
